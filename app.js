a54173b3a816dcb5415cdbae8cc273e745643b13
Franks2695 <frankssanay@gmail.com>
2021-06-14 00:18:51 -0500

    Muestra todos lo datos

--- app.js
+++ app.js
@@ -22,24 +22,44 @@ clima.getClima(ciudad)
             console.log();
             console.log(`------------- ${ciudad} -------------`.bgMagenta);
             console.log();
-            const [lon, lat, temp, feels, hum, pais] = respuesta;
-            console.log('PAÍS =', pais);
+            const [lon, lat, idw, main, desc, icon, base, temp, feels, tmin, tmax, pres, hum, seal, grandl, visi, speedw, degw, gustw, cloud, dt, type, idp, pais, timez, idg, cod] = respuesta;
             console.log('LONGITUD =', lon);
             console.log('LATITUD =', lat);
-            console.log('HUMEDAD =', hum);
+            console.log('ID CLIMA =', idw);
+            console.log('ESTADO =', main);
+            console.log('DESCRIPCIÓN =', desc);
+            console.log('ICONO =', icon);
+            console.log('BASE =', base);
             console.log();
             console.log('FRIO =', '  '.bgBlue, '  TEMPLADO =', '  '.inverse.white, '  CALIENTE = ', '  '.bgRed);
             console.log();
             if (temp <= 15) {
                 console.log(colors.bgBlue('TEMPERATURA =', temp, 'C°'));
-                console.log(colors.bgBlue('PERO SE SIENTE COMO DE =', feels, 'C°'));
             } else if (temp > 15 && temp <= 22) {
                 console.log(colors.inverse.white('TEMPERATURA =', temp, 'C°'));
-                console.log(colors.inverse.white('PERO SE SIENTE COMO DE =', feels, 'C°'));
             } else if (temp > 22) {
                 console.log(colors.bgRed('TEMPERATURA =', temp, 'C°'));
-                console.log(colors.bgRed('PERO SE SIENTE COMO DE =', feels, 'C°'));
             }
+            console.log();
+            console.log('PERO SE SIENTE COMO DE =', feels, 'C°');
+            console.log('TEMPERATURA MÍNIMA =', tmin);
+            console.log('TEMPERATURA MÁXIMA =', tmax);
+            console.log('PRESIÓN =', pres);
+            console.log('HUMEDAD =', hum);
+            console.log('NIVEL DEL MAR =', seal);
+            console.log('NIVEL DE LA TIERRA =', grandl);
+            console.log('VISIBILIDAD =', visi);
+            console.log('VELOCIDAD DEL VIENTO =', speedw);
+            console.log('GRADOS DEL VIENTO =', degw);
+            console.log('RÁFAGA DEL VIENTO =', gustw);
+            console.log('NUBES =', cloud);
+            console.log('DT =', dt);
+            console.log('TIPO =', type);
+            console.log('ID PAÍS =', idp);
+            console.log('PAÍS =', pais);
+            console.log('ZONA HORARIA =', timez);
+            console.log('ID GENERAL =', idg);
+            console.log('CÓDIGO =', cod);
         }
     }).catch(err => {
         console.log(err);
--- controlador/clima.js
+++ controlador/clima.js
@@ -8,7 +8,7 @@ const getClima = async(ciudad) => {
     try {
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`
         const respuesta = await axios.get(url);
-        return [respuesta.data.coord.lon, respuesta.data.coord.lat, respuesta.data.main.temp, respuesta.data.main.feels_like, respuesta.data.main.humidity, respuesta.data.sys.country];
+        return [respuesta.data.coord.lon, respuesta.data.coord.lat, respuesta.data.weather[0].id, respuesta.data.weather[0].main, respuesta.data.weather[0].description, respuesta.data.weather[0].icon, respuesta.data.base, respuesta.data.main.temp, respuesta.data.main.feels_like, respuesta.data.main.temp_min, respuesta.data.main.temp_max, respuesta.data.main.pressure, respuesta.data.main.humidity, respuesta.data.main.sea_level, respuesta.data.main.grnd_level, respuesta.data.visibility, respuesta.data.wind.speed, respuesta.data.wind.deg, respuesta.data.wind.gust, respuesta.data.clouds.all, respuesta.data.dt, respuesta.data.sys.type, respuesta.data.sys.id, respuesta.data.sys.country, respuesta.data.timezone, respuesta.data.id, respuesta.data.cod];
     } catch (error) {
         console.log("------- ERROR -------".red, error.data);
     }
